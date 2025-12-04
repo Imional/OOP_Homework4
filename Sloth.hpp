9ca121576dc308e597845a4aa8eb736ecbd2336b
Imional <100889680+Imional@users.noreply.github.com>
2025-12-04 17:50:55 +0900

    Update Sloth.hpp

--- Sloth.hpp
+++ Sloth.hpp
@@ -4,9 +4,11 @@
 
 struct Sloth {
     Herbivore parentStruct;
-    bool canMove; 
+    bool isSkipping; // State to track turn skipping
 };
 
-void Sloth_init(Sloth* s, int x, int y, int*** map);
+void Sloth_observe(Animal* self);
+int Sloth_move(Animal* self);
+Animal* Sloth_giveBirth(Animal* self);
 
-#endif
+#endif // SLOTH_HPP
