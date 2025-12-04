306ed86fe94b5062fa797d40bb0147d2b0f12231
Imional <100889680+Imional@users.noreply.github.com>
2025-12-04 17:51:27 +0900

    Update Carnivore.hpp

--- Carnivore.hpp
+++ Carnivore.hpp
@@ -1,12 +1,13 @@
 #ifndef CARNIVORE_HPP
 #define CARNIVORE_HPP
-
 #include "Animal.hpp"
 
 struct Carnivore {
     Animal parent;
 };
 
-void Carnivore_init(Carnivore* c, int x, int y, int*** map);
+void Carnivore_observe(Animal* self);
+int Carnivore_move(Animal* self);
+Animal* Carnivore_giveBirth(Animal* self);
 
-#endif
+#endif // CARNIVORE_HPP
