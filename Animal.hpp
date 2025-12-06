8529c05e27fe74228e3dccc88c62c3f90194d0c7
Imional <100889680+Imional@users.noreply.github.com>
2025-12-06 18:03:05 +0900

    Update Animal.hpp

--- Animal.hpp
+++ Animal.hpp
@@ -4,7 +4,6 @@
 #include "Utils.hpp"
 
 struct Animal {
-    // Attributes
     Location loc;
     int energy;
     int movingCost;
@@ -13,29 +12,24 @@ struct Animal {
     int energyValue;
     int viewRange;
     
-    int** viewArray; // 2D array allocated dynamically
-    int*** map;      // Pointer to the global map pointer
+    int** viewArray;
+    int*** map;
+    
+    int animalID; // Used for map tracking
+    int typeID;   // 1=Herb, 2=Carn, etc.
 
-    // Helpers for ID tracking (needed for map updates)
-    int animalID;    
-    int typeID;      // 0:Herb, 1:Carn, 2:Omni (used for basic ID generation)
-
-    // Behavior Function Pointers
     void (*observe)(Animal* self);
-    int (*move)(Animal* self); // Returns index of eaten animal, 100000 if none.
+    int (*move)(Animal* self); // Returns index of eaten animal, or 100000.
     Animal* (*giveBirth)(Animal* self);
 
-    // Helper to free viewArray
     void cleanup() {
         if (viewArray) {
             int size = 2 * viewRange + 1;
-            for (int i = 0; i < size; ++i) {
-                delete[] viewArray[i];
-            }
+            for(int i=0; i<size; ++i) delete[] viewArray[i];
             delete[] viewArray;
             viewArray = nullptr;
         }
     }
 };
 
-#endif // ANIMAL_HPP
+#endif
